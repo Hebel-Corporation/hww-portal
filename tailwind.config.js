7605729fcfff818eadf332f5cb756a86633f9c06
bienfaitLuvako <bienfaitluvako@gmail.com>
2024-09-12 14:59:52 +0200

    init hww portal project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: { colors: {
+      primary1: "rgba(6,146,207)",
+      primary2: "rgba(107,215,40)",
+      primary3: "rgba(247,184,5)",
+  }},
+  },
+  plugins: [],
+}
+
